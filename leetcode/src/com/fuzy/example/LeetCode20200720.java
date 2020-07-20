eb73bfdaa3064e14dd5a098bfa3f9d14c76b2189
fuzy <fuzy@ufen.cn>
2020-07-20 14:40:43 +0800

    2020-07-20题目

---
+++ leetcode/src/com/fuzy/example/LeetCode20200720.java
@@ -0,0 +1,37 @@
+package com.fuzy.example;
+
+/**
+ * @author fuzy
+ * @version 1.0.0
+ * @Description
+ * @date 2020/7/20 10:38
+ */
+public class LeetCode20200720 {
+    public static void main(String[] args) {
+        int[] nums = {3,2,2,3};
+        int val = 2;
+        int i = removeElement1(nums, val);
+        System.out.println(i);
+    }
+
+    public static int removeElement1(int[] nums, int val) {
+        int q = 0;
+        for (int i = 0; i < nums.length;i++) {
+            if(nums[i]==val){
+                for (int j=i+1;j<nums.length;j++){
+                    if(nums[j]!=val){
+                        int temp = nums[i];
+                        nums[i]=nums[j];
+                        nums[j]=temp;
+                        q++;
+                        break;
+                    }
+                }
+            }else{
+                q++;
+            }
+        }
+
+        return q;
+    }
+}
