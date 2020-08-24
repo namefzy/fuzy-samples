5f569558f05c5adb6832594739f858e931d6afe8
fzy <1156491161@qq.com>
2020-08-24 22:56:44 +0800

    2020-08-24

--- leetcode/src/com/fuzy/example/leetcode/editor/cn/[108]将有序数组转换为二叉搜索树.java
+++ leetcode/src/com/fuzy/example/leetcode/editor/cn/[108]将有序数组转换为二叉搜索树.java
@@ -37,9 +37,17 @@ class Solution33 {
      * @return
      */
     public TreeNode sortedArrayToBST(int[] nums) {
-        int mid = (nums.length-1)/2;
-
-        return null;
+        return helper(nums,0,nums.length);
+    }
+    public TreeNode helper(int[] nums, int left, int right) {
+        if(left>=right){
+            return null;
+        }
+        int mid = (left+right)/2;
+        TreeNode treeNode = new TreeNode(nums[mid]);
+        treeNode.left = helper(nums,left,mid);
+        treeNode.right = helper(nums,mid+1,right);
+        return treeNode;
     }
     public class TreeNode {
       int val;
