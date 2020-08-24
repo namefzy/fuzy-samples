2ede746cea2abdc0d33e634ff52e0e38b35c03fb
fuzy <fuzy@ufen.cn>
2020-08-24 11:31:45 +0800

    2020-08-24

---
+++ leetcode/src/com/fuzy/example/leetcode/editor/cn/[107]二叉树的层次遍历 II.java
@@ -0,0 +1,109 @@
+package com.fuzy.example.leetcode.editor.cn;
+//给定一个二叉树，返回其节点值自底向上的层次遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+//
+// 例如： 
+//给定二叉树 [3,9,20,null,null,15,7], 
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+// 
+//
+// 返回其自底向上的层次遍历为： 
+//
+// [
+//  [15,7],
+//  [9,20],
+//  [3]
+//]
+// 
+// Related Topics 树 广度优先搜索 
+// 👍 290 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution32 {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            List<Integer> tmp = new ArrayList<>();
+            int len = q.size();
+            for (int i = 0; i < len; i++) {
+                TreeNode node = q.poll();
+                tmp.add(node.val);
+                if (node.left != null) {
+                    q.add(node.left);
+                }
+                if (node.right != null) {
+                    q.add(node.right);
+                }
+            }
+            // 在索引 0 的位置加入一维数组 tmp
+            // 每次新的数组进来都会被放在开始的位置
+            ans.add(0, tmp);
+        }
+        return ans;
+
+    }
+
+    /**
+     * 递归问题所在，顺序问题
+     * @param root
+     * @param list
+     * @return
+     */
+    public List<List<Integer>> digui(TreeNode root,List<List<Integer>> list){
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        List<Integer> shortList = new ArrayList<>();
+
+        if(left==null&&right==null){
+            return list;
+        }
+
+        if(left!=null){
+            shortList.add(left.val);
+            digui(left,list);
+        }
+        if(right!=null){
+            shortList.add(right.val);
+            digui(right,list);
+        }
+        list.add(shortList);
+
+        return list;
+
+    }
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
