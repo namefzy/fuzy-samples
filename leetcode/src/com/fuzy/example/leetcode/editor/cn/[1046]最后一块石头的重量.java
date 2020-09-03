86c76a120bd9a39719bf30d22066d5991bf456c2
fuzy <fuzy@ufen.cn>
2020-09-03 14:26:13 +0800

    2020-09-03

---
+++ leetcode/src/com/fuzy/example/leetcode/editor/cn/[1046]最后一块石头的重量.java
@@ -0,0 +1,81 @@
+package com.fuzy.example.leetcode.editor.cn;
+//有一堆石头，每块石头的重量都是正整数。
+//
+// 每一回合，从中选出两块 最重的 石头，然后将它们一起粉碎。假设石头的重量分别为 x 和 y，且 x <= y。那么粉碎的可能结果如下： 
+//
+// 
+// 如果 x == y，那么两块石头都会被完全粉碎； 
+// 如果 x != y，那么重量为 x 的石头将会完全粉碎，而重量为 y 的石头新重量为 y-x。 
+// 
+//
+// 最后，最多只会剩下一块石头。返回此石头的重量。如果没有石头剩下，就返回 0。 
+//
+// 
+//
+// 示例： 
+//
+// 输入：[2,7,4,1,8,1]
+//输出：1
+//解释：
+//先选出 7 和 8，得到 1，所以数组转换为 [2,4,1,1,1]，
+//再选出 2 和 4，得到 2，所以数组转换为 [2,1,1,1]，
+//接着是 2 和 1，得到 1，所以数组转换为 [1,1,1]，
+//最后选出 1 和 1，得到 0，最终数组转换为 [1]，这就是最后剩下那块石头的重量。 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= stones.length <= 30 
+// 1 <= stones[i] <= 1000 
+// 
+// Related Topics 堆 贪心算法 
+// 👍 72 👎 0
+
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution44 {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2-o1;
+            }
+        });
+        queue.offer(2);
+        queue.offer(1);
+        queue.offer(3);
+        for (int i = 0; i < queue.size(); i++) {
+            System.out.println(queue.poll());
+        }
+    }
+    public int lastStoneWeight(int[] stones) {
+        Queue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2-o1;
+            }
+        });
+        for (int i = 0; i < stones.length; i++) {
+            queue.offer(stones[i]);
+        }
+        while (queue.size()>1){
+            Integer p1 = queue.poll();
+            Integer p2 = queue.poll();
+            int value = Math.abs(p1 - p2);
+            if(value!=0){
+                queue.offer(value);
+            }
+        }
+        if (queue.isEmpty()){
+            return 0;
+        }
+        return queue.peek();
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
