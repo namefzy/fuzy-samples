17d74e608819909f6de2666665cb07d2ade87366
fzy <1156491161@qq.com>
2020-09-09 21:57:17 +0800

    2020-09-09

--- leetcode/src/com/fuzy/example/leetcode/editor/cn/[110]平衡二叉树.java
+++ leetcode/src/com/fuzy/example/leetcode/editor/cn/[110]平衡二叉树.java
@@ -53,7 +53,23 @@ package com.fuzy.example.leetcode.editor.cn;
 class Solution41 {
     public boolean isBalanced(TreeNode root) {
 
-        return true;
+        return recur(root)!=-1;
+    }
+
+    private int recur(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        //递归左节点
+        int left = recur(root.left);
+        if(left==-1){
+            return -1;
+        }
+        int right = recur(root.right);
+        if(right==-1){
+            return -1;
+        }
+        return Math.abs(left-right)<2?Math.max(left,right)+1:-1;
     }
 
     public class TreeNode {
