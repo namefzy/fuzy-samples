ef1c93e69516c8eedd09012ccaac27c3ae6898e8
fzy <1156491161@qq.com>
2020-10-26 21:06:03 +0800

    2020-10-26

---
+++ leetcode/src/com/fuzy/example/leetcode/editor/cn/[172]阶乘后的零.java
@@ -0,0 +1,32 @@
+package com.fuzy.example.leetcode.editor.cn;//给定一个整数 n，返回 n! 结果尾数中零的数量。
+//
+// 示例 1: 
+//
+// 输入: 3
+//输出: 0
+//解释: 3! = 6, 尾数中没有零。 
+//
+// 示例 2: 
+//
+// 输入: 5
+//输出: 1
+//解释: 5! = 120, 尾数中有 1 个零. 
+//
+// 说明: 你算法的时间复杂度应为 O(log n) 。 
+// Related Topics 数学 
+// 👍 376 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution73 {
+    public int trailingZeroes(int n) {
+        int count = 0;
+        while (n > 0) {
+            count += n / 5;
+            n = n / 5;
+        }
+        return count;
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
