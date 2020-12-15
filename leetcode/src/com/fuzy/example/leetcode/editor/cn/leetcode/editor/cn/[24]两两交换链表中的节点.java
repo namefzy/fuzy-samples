35d3a63a3003cfe6953e037e2515b6cdf05d94bb
namefzy <1156491161@qq.com>
2020-12-15 21:53:27 +0800

    2020-12-15 中序遍历、翻转链表

---
+++ leetcode/src/com/fuzy/example/leetcode/editor/cn/leetcode/editor/cn/[24]两两交换链表中的节点.java
@@ -0,0 +1,86 @@
+package com.fuzy.example.leetcode.editor.cn.leetcode.editor.cn;
+//给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+//
+// 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：head = [1,2,3,4]
+//输出：[2,1,4,3]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = []
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = [1]
+//输出：[1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点的数目在范围 [0, 100] 内 
+// 0 <= Node.val <= 100 
+// 
+// Related Topics 链表 
+// 👍 760 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution111 {
+    public ListNode swapPairs(ListNode head){
+        ListNode pre = new ListNode(0);
+        pre.next = head;
+        ListNode temp = pre;
+        while (temp.next!=null&&temp.next.next!=null){
+            ListNode start = temp.next;
+            ListNode end = temp.next.next;
+            temp.next = end;
+            start.next = end.next;
+            end.next = start;
+            temp = start;
+        }
+        return pre.next;
+    }
+    public ListNode swapPairs1(ListNode head) {
+        if(head==null||head.next==null){
+            return head;
+        }
+        ListNode next = head.next;
+        head.next = swapPairs1(next.next);
+        next.next = head;
+
+        return next;
+    }
+
+  public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+}
+//leetcode submit region end(Prohibit modification and deletion)
