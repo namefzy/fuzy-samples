461a1519b884c24766f30d802adba2780fd9f816
namefzy <1156491161@qq.com>
2021-03-10 21:38:10 +0800

    2021-03-10 删除排序数组中的重复项

---
+++ leetcode/src/com/fuzy/example/leetcode/editor/cn/[80]删除排序数组中的重复项 II.java
@@ -0,0 +1,79 @@
+package com.fuzy.example.leetcode.editor.cn;//给定一个增序排列数组 nums ，你需要在 原地 删除重复出现的元素，使得每个元素最多出现两次，返回移除后数组的新长度。
+//
+// 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。 
+//
+// 
+//
+// 说明： 
+//
+// 为什么返回数值是整数，但输出的答案是数组呢？ 
+//
+// 请注意，输入数组是以“引用”方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。 
+//
+// 你可以想象内部操作如下： 
+//
+// 
+//// nums 是以“引用”方式传递的。也就是说，不对实参做任何拷贝
+//int len = removeDuplicates(nums);
+//
+//// 在函数里修改输入数组对于调用者是可见的。
+//// 根据你的函数返回的长度, 它会打印出数组中该长度范围内的所有元素。
+//for (int i = 0; i < len; i++) {
+//    print(nums[i]);
+//} 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [1,1,1,2,2,3]
+//输出：5, nums = [1,1,2,2,3]
+//解释：函数应返回新长度 length = 5, 并且原数组的前五个元素被修改为 1, 1, 2, 2, 3 。 你不需要考虑数组中超出新长度后面的元素。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [0,0,1,1,1,1,2,3,3]
+//输出：7, nums = [0,0,1,1,2,3,3]
+//解释：函数应返回新长度 length = 7, 并且原数组的前五个元素被修改为 0, 0, 1, 1, 2, 3, 3 。 你不需要考虑数组中超出新长度后面
+//的元素。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= nums.length <= 3 * 104 
+// -104 <= nums[i] <= 104 
+// nums 按递增顺序排列 
+// 
+// Related Topics 数组 双指针 
+// 👍 375 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution800 {
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        int slow = 0, fast = 1;//定义快慢指针
+        int count = 0;
+        while(fast < n){
+            if(nums[fast] == nums[slow]){
+                count++;//如果相邻的两个数相等，计数器+1
+            }else{
+                count = 0;// 如果不相等复位为0
+            }
+            if(count < 2){//如果计数器小于2，则保留
+                slow++;//当前符合条件的数组的长度
+                nums[slow] = nums[fast];
+            }
+            fast++;
+        }
+        return slow + 1;
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
