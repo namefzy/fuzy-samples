653f4c3a4ca3fb4736c837c211e470043eaa7caf
fuzy <fuzy@ufen.cn>
2021-03-31 15:38:44 +0800

    2021-03-29 验证二叉搜索树

--- leetcode/src/com/fuzy/example/leetcode/editor/cn/[96]不同的二叉搜索树.java
+++ leetcode/src/com/fuzy/example/leetcode/editor/cn/[96]不同的二叉搜索树.java
@@ -24,7 +24,7 @@ class Solution960 {
         int[] dp = new int[n+1];
         dp[0] = 1;
         dp[1] = 1;
-
+        //G(2) = G(1)*G(0)+G(0)*G(1)
         for(int i = 2; i < n + 1; i++)
             for(int j = 1; j < i + 1; j++)
                 dp[i] += dp[j-1] * dp[i-j];
