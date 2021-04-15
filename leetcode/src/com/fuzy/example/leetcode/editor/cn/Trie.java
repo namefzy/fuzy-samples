69dbf43a9422e4787633d3f9e62ad6f16aa5039c
fuzy <fuzy@ufen.cn>
2021-04-15 17:03:22 +0800

    2021-04-15 前缀树抄袭版本

---
+++ leetcode/src/com/fuzy/example/leetcode/editor/cn/Trie.java
@@ -0,0 +1,58 @@
+package com.fuzy.example.leetcode.editor.cn;
+
+/**
+ * @author fuzy
+ * @version 1.0
+ * @Description
+ * @company 上海有分科技发展有限公司
+ * @email fuzy@ufen.cn
+ * @date 2021/4/15 12:37
+ */
+public class Trie {
+
+    class TireNode{
+       private boolean isEnd;
+       TireNode[] next;
+       public TireNode(){
+           isEnd = false;
+           next = new TireNode[26];
+       }
+    }
+
+    private TireNode root;
+
+    public Trie(){
+        root = new TireNode();
+    }
+
+    public void insert(String word){
+        TireNode node = root;
+        for (char c : word.toCharArray()) {
+            if(node.next[c-'a']==null){
+                node.next[c-'a'] = new TireNode();
+            }
+            node = node.next[c-'a'];
+        }
+        node.isEnd = true;
+    }
+    public boolean search(String word){
+        TireNode node = root;
+        for (char c : word.toCharArray()) {
+            node = node.next[c-'a'];
+            if(node==null){
+                return false;
+            }
+        }
+        return node.isEnd;
+    }
+    public boolean startsWith(String prefix) {
+        TireNode node = root;
+        for (char c : prefix.toCharArray()) {
+            node = node.next[c - 'a'];
+            if (node == null) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
