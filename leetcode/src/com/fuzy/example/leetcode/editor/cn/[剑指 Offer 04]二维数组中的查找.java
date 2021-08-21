1fc981066a5fe0c2870035d0ec7b2b19a11335b0
fuzy <fuzy@fosun.com>
2021-08-21 10:20:23 +0800

    2021-08-21 剑指offer系列 二维数组中的查找 优化

--- leetcode/src/com/fuzy/example/leetcode/editor/cn/[剑指 Offer 04]二维数组中的查找.java
+++ leetcode/src/com/fuzy/example/leetcode/editor/cn/[剑指 Offer 04]二维数组中的查找.java
@@ -39,6 +39,21 @@ package com.fuzy.example.leetcode.editor.cn;
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution132 {
+
+    public boolean findNumberIn2DArray2(int[][] matrix,int target){
+        int i = matrix.length-1,j=0;
+        while (i>=0&&j<matrix[0].length){
+            if(matrix[i][j]>target){
+                i--;
+            }else if(matrix[i][j]<target){
+                j++;
+            }else {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public boolean findNumberIn2DArray(int[][] matrix, int target) {
         if(matrix.length==0){
             return false;
