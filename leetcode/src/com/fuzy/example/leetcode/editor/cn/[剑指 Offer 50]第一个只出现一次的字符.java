f4d662d592a96a400f563ef448f9772740bebaf3
fuzy <fuzy@fosun.com>
2021-08-22 11:22:41 +0800

    2021-08-22 剑指offer系列 第一个只出现一次的字符

---
+++ leetcode/src/com/fuzy/example/leetcode/editor/cn/[剑指 Offer 50]第一个只出现一次的字符.java
@@ -0,0 +1,41 @@
+package com.fuzy.example.leetcode.editor.cn;//在字符串 s 中找出第一个只出现一次的字符。如果没有，返回一个单空格。 s 只包含小写字母。
+//
+// 示例: 
+//
+// s = "abaccdeff"
+//返回 "b"
+//
+//s = "" 
+//返回 " "
+// 
+//
+// 
+//
+// 限制： 
+//
+// 0 <= s 的长度 <= 50000 
+// Related Topics 队列 哈希表 字符串 计数 
+// 👍 128 👎 0
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class SolutionOffer50 {
+    public char firstUniqChar(String s) {
+        Map<Character, Integer> frequency = new HashMap<Character, Integer>();
+        for (int i = 0; i < s.length(); ++i) {
+            char ch = s.charAt(i);
+            frequency.put(ch, frequency.getOrDefault(ch, 0) + 1);
+        }
+        for (int i = 0; i < s.length(); ++i) {
+            if (frequency.get(s.charAt(i)) == 1) {
+                return s.charAt(i);
+            }
+        }
+        return ' ';
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
