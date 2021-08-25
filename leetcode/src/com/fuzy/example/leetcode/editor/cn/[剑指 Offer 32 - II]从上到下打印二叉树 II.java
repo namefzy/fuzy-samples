89e4b8442633ccf38af493584fed3a972765ce48
fuzy <fuzy@fosun.com>
2021-08-25 08:18:24 +0800

    2021-08-25 剑指offer系列 从上到下打印二叉树02

---
+++ leetcode/src/com/fuzy/example/leetcode/editor/cn/[剑指 Offer 32 - II]从上到下打印二叉树 II.java
@@ -0,0 +1,82 @@
+package com.fuzy.example.leetcode.editor.cn;//从上到下按层打印二叉树，同一层的节点按从左到右的顺序打印，每一层打印到一行。
+//
+// 
+//
+// 例如: 
+//给定二叉树: [3,9,20,null,null,15,7], 
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+// 
+//
+// 返回其层次遍历结果： 
+//
+// [
+//  [3],
+//  [9,20],
+//  [15,7]
+//]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 节点总数 <= 1000 
+// 
+//
+// 注意：本题与主站 102 题相同：https://leetcode-cn.com/problems/binary-tree-level-order-tra
+//versal/ 
+// Related Topics 树 广度优先搜索 二叉树 
+// 👍 126 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+class SolutionOffer3202 {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> list = new ArrayList<>();
+        helper(list,0,root);
+        return list;
+    }
+
+    public void helper(List<List<Integer>> list,int index,TreeNode node){
+        if(node==null){
+            return;
+        }
+        if(list.size()<=index){
+            list.add(new ArrayList<>());
+        }
+        list.get(index).add(node.val);
+        helper(list,index+1,node.left);
+        helper(list,index+1,node.right);
+
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
