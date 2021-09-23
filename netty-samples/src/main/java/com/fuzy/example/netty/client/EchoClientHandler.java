46a73d51788c28798be63f51cba86f40b0a47867
fuzy <fuzy@fosun.com>
2021-09-23 22:38:25 +0800

    2021-09-23 netty示例

---
+++ netty-samples/src/main/java/com/fuzy/example/netty/client/EchoClientHandler.java
@@ -0,0 +1,43 @@
+package com.fuzy.example.netty.client;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.CharsetUtil;
+
+/**
+ * @author fuzy
+ * @date 2021/9/23 21:39
+ */
+public class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
+
+    /**
+     * 当被是活跃的时候，发送一条消息
+     * @param ctx
+     * @throws Exception
+     */
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(Unpooled.copiedBuffer("Netty rocks!", CharsetUtil.UTF_8));
+    }
+
+    /**
+     * 记录已接受消息的转储
+     * @param channelHandlerContext
+     * @param byteBuf
+     * @throws Exception
+     */
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf) throws Exception {
+        System.out.println("Client received:"+byteBuf.toString(CharsetUtil.UTF_8));
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+
+}
